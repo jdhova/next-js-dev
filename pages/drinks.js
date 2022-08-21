36637100aded5e85d3cc4a06c82ba3a34613af68
Jude Okagu <juud0010@gmail.com>
2022-08-21 13:54:07 -0400

    fixing

--- pages/drinks.js
+++ pages/drinks.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.css';
 import drinksStyles from '../styles/Drinks.module.css';
-import img1 from "./images/chai.jpg";
-import img2 from "./images/lassi.jpg";
-import img3 from "./images/jaljeera.jpg";
-import img4 from "./images/thandai.jpg";
+import img1 from "./images/t-nuts.jpeg";
+import img2 from "./images/zobo.jpeg";
+import img3 from "./images/ginger.jpeg";
+import img4 from "./images/aam.jpg";
 import img5 from "./images/aam.jpg";
 import img6 from "./images/coffee.jpg";
 import { Row, Col, Nav } from "reactstrap";
@@ -19,21 +19,21 @@ const drinks = () => {
           <div className="justify-content-center">
             <Image alt="description of image" src={img1} className={'rounded'}/>
           </div>
-          <div className="card-body text-center h3">Chai</div>
+          <div className="card-body text-center h3">T nuts</div>
           <button className="btn btn-primary btn-block">Order Now</button>
         </Col>
         <Col>
           <div className="justify-content-center">
             <Image alt="description of image" src={img2} className={'rounded'} />
           </div>
-          <div className="card-body text-center h3">Lassi</div>
+          <div className="card-body text-center h3">Zobbo</div>
           <button className="btn btn-primary btn-md">Order Now</button>
         </Col>
         <Col>
           <div className="justify-content-center">
             <Image alt="description of image" src={img3} className={'rounded'} />
           </div>
-          <div className="card-body text-center h3">Jaljeera</div>
+          <div className="card-body text-center h3">Kunu</div>
           <button className="btn btn-primary btn-md">Order Now</button>
         </Col>
         <div className="w-100"></div>
@@ -41,14 +41,14 @@ const drinks = () => {
           <div className="justify-content-center ">
             <Image alt="description of image" src={img4} className={'rounded'} />
           </div>
-          <div className="card-body text-center h3">Thandai</div>
+          <div className="card-body text-center h3">Ginger</div>
           <button className="btn btn-primary btn-md">Order Now</button>
         </Col>
         <Col>
           <div className="justify-content-center ">
             <Image alt="description of image" src={img5} className={'rounded'} />
           </div>
-          <div className="card-body text-center h3">Panna</div>
+          <div className="card-body text-center h3">Ginger and Garlic</div>
           <button className="btn btn-primary btn-md">Order Now</button>
         </Col>
         <Col>
